e275c3f439ae4b90055289589bf73e98c6655b0a
NaraeJang <j.naaaarae@gmail.com>
2023-07-04 15:48:59 -0400

    JS:select elements

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   );
 }
+
+function Gallery(element) {
+  this.list = [...element.querySelectorAll(".img")]; //ES6 Feature: [... the items that you want to copy and paste in array]
+  console.log(this.list);
+  // target
+  this.modal = getElement(".modal");
+  this.modalImg = getElement(".main-img");
+  this.modalImages = getElement(".modal-img");
+  this.closeBtn = getElement(".close-btn");
+  this.nextBtn = getElement(".next-btn");
+  this.prevBtn = getElement(".prev-btn");
+}
+
+const nature = new Gallery(getElement(".nature")),
+  city = new Gallery(getElement(".city"));
