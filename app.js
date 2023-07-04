5826fdd0650b6923bc8a02426f259b3ec51a10aa
NaraeJang <j.naaaarae@gmail.com>
2023-07-04 16:18:16 -0400

    JS:open modal setup

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ function getElement(selection) {
 }
 
 function Gallery(element) {
+  this.container = element;
   this.list = [...element.querySelectorAll(".img")]; //ES6 Feature: [... the items that you want to copy and paste in array]
-  console.log(this.list);
   // target
   this.modal = getElement(".modal");
   this.modalImg = getElement(".main-img");
@@ -18,7 +18,24 @@ function Gallery(element) {
   this.closeBtn = getElement(".close-btn");
   this.nextBtn = getElement(".next-btn");
   this.prevBtn = getElement(".prev-btn");
+  // self ref
+  // let self = this; people uses this method a lot. but we are going to use bind() here.
+  // bind functions
+  // this.openModal = this.openModal.bind(this); //By using bind(), we can indicate Gallery so that we have access to its values.
+  // container event
+  this.container.addEventListener(
+    "click",
+    function (e) {
+      this.openModal();
+    }.bind(this)
+  );
 }
+
+Gallery.prototype.openModal = function () {
+  console.log(this);
+  console.log("open modal");
+  this.modal.classList.add("open");
+};
 
 const nature = new Gallery(getElement(".nature")),
   city = new Gallery(getElement(".city"));
